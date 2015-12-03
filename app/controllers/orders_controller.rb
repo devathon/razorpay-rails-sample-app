10ac45f02a0d2c1862ffa07a2c1949b7e23ee17e
jnan <jnaneshwar@devathon.com>
2015-12-03 20:07:05 +0530

    updated listing orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,12 @@
 class OrdersController < ApplicationController
   skip_before_action :verify_authenticity_token
   def purchase_status
+    binding.pry
     @payment_id = payment_params['razorpay_payment_id']
     @amount = get_payment_amount(@payment_id)
-    payment_status=get_payment_status(@payment_id)
-    @status = ""
-    if payment_status 
-      flash.now[:alert] = "Payment failed try again"
-      redirect_to root_url
-      @status = "failed"
-    else
-      captured=capture_payment(@payment_id)
-      flash.now[:notice] = "Payment successful"
-      @status = "successful"
-    end
+    captured=capture_payment(@payment_id)
+    @status=get_payment_status(@payment_id)
     @order = Order.create(product_id: payment_params[:product_id],payment_id: payment_params[:razorpay_payment_id],status: @status,price: @amount)
-    #render 'order_status'
     redirect_to :action => "show", :id => @order.id
   end
 
@@ -26,8 +17,8 @@ class OrdersController < ApplicationController
 
   def index
     @orders = Order.all
-    @success_orders=Order.where(:status => "captured")
-    @refunded_orders=Order.where(:status => "refunded")
+    @success_orders=Order.where(:status => "authorized")
+    @captured_orders=Order.where(:status => "captured")
     @failed_orders=Order.where(:status => "failed")
   end
 
