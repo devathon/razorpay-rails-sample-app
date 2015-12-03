3cd9e9cb9c93482eb2a07f1383fa2f58b3bc05ea
jnan <jnaneshwar@devathon.com>
2015-12-03 18:44:26 +0530

    added product page and product list page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,36 @@
 class OrdersController < ApplicationController
   skip_before_action :verify_authenticity_token
   def purchase_status
-    #binding.pry
     @payment_id = payment_params['razorpay_payment_id']
+    @amount = get_payment_amount(@payment_id)
     payment_status=get_payment_status(@payment_id)
+    @status = ""
     if payment_status 
       flash.now[:alert] = "Payment failed try again"
       redirect_to root_url
+      @status = "failed"
     else
       captured=capture_payment(@payment_id)
       flash.now[:notice] = "Payment successful"
+      @status = "successful"
     end
-    render 'order_status'
+    @order = Order.create(product_id: payment_params[:product_id],payment_id: payment_params[:razorpay_payment_id],status: @status,price: @amount)
+    #render 'order_status'
+    redirect_to :action => "show", :id => @order.id
   end
 
+  def show
+    @order = Order.find_by_id(params[:id])
+    @product = Product.find_by_id(@order.product_id)
+  end
+
+  def index
+    @orders = Order.all
+  end
 
   private
     def payment_params
-      params.permit(:razorpay_payment_id, :user_id, :product_id)
+      params.permit(:razorpay_payment_id, :user_id, :product_id,:price)
     end
 
 end
--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -2,11 +2,11 @@ module OrdersHelper
   def get_payment_status(raz_payment_id)
     Razorpay::Payment.fetch(raz_payment_id).status == "failed"
   end
-
+  def get_payment_amount(raz_payment_id)
+  	Razorpay::Payment.fetch(raz_payment_id).amount
+  end
   def capture_payment(raz_payment_id)
-    amount=Razorpay::Payment.fetch(raz_payment_id).amount
+  	amount = get_payment_amount(raz_payment_id)
     Razorpay::Payment.fetch(raz_payment_id).capture({amount: amount})
   end
-
-
 end
