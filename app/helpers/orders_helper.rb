1d6067221f2ca40b44e293237991c52654389bd0
ravikumarj <ravikumar@crypsis.net>
2015-12-03 16:43:09 +0530

    payment inspection methods are added to order_helper

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,12 @@
 module OrdersHelper
+  def get_payment_status(raz_payment_id)
+    Razorpay::Payment.fetch(raz_payment_id).status == "failed"
+  end
+
+  def capture_payment(raz_payment_id)
+    amount=Razorpay::Payment.fetch(raz_payment_id).amount
+    Razorpay::Payment.fetch(raz_payment_id).capture({amount: amount})
+  end
+
+
 end
