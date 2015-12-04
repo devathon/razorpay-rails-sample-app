d48a1beff7f24a95da6de04f97df7ce790ea2079
jnan <jnaneshwar@devathon.com>
2015-12-04 17:00:55 +0530

    added user_id to order

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203091239) do
+ActiveRecord::Schema.define(version: 20151204112443) do
 
   create_table "orders", force: :cascade do |t|
     t.integer  "product_id"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20151203091239) do
     t.string   "payment_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "products", force: :cascade do |t|
