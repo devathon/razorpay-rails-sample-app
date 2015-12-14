cc2a53efd06dbafe7da15e7b18dc3d847b27cd62
ravikumarj <ravikumar@crypsis.net>
2015-12-14 15:28:54 +0530

    Updated readme, seed & Razorpayconcerns

--- app/models/concerns/order_concerns/razorpay.rb
+++ app/models/concerns/order_concerns/razorpay.rb
@@ -1,10 +1,10 @@
 module OrderConcerns::Razorpay
-    extend ActiveSupport::Concern
-    included do
-      class << self
-        def fetch_payment(raz_payment_id)
-          Razorpay::Payment.fetch(raz_payment_id)
-        end
+  extend ActiveSupport::Concern
+  included do
+    class << self
+      def fetch_payment(raz_payment_id)
+        Razorpay::Payment.fetch(raz_payment_id)
       end
+    end
   end
 end
